d164a70f07971f4aa0a8cb86db253cf61894f565
louis <Louiis1997@hotmail.fr>
2022-05-08 12:21:59 +0200

    Start Jee project

---
+++ src/main/java/com/esgi/algoBattle/AlgoBattle.java
@@ -0,0 +1,12 @@
+package com.esgi.algoBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoBattle {
+    public static void main(String[] args) {
+        System.out.println("Hello Spring!");
+        SpringApplication.run(AlgoBattle.class, args);
+    }
+}
