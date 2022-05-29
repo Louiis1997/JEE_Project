3f92f1c25cd62312ac9a9121e49e470cf8363596
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-05-29 22:12:34 +0200

    update model

--- src/main/java/com/esgi/algoBattle/algorithm_case/domain/model/AlgorithmCase.java
+++ src/main/java/com/esgi/algoBattle/algorithm_case/domain/model/AlgorithmCase.java
@@ -1,4 +1,20 @@
 package com.esgi.algoBattle.algorithm_case.domain.model;
 
+import com.esgi.algoBattle.algorithm.domain.model.Algorithm;
+import com.esgi.algoBattle.case_input.domain.model.CaseInput;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+
+@Accessors(chain = true)
+@Data
 public class AlgorithmCase {
+    private Long id;
+    private String name;
+    private List<CaseInput> input;
+    private String outputType;
+    private String expectedOutput;
+    private Algorithm algorithm;
 }
