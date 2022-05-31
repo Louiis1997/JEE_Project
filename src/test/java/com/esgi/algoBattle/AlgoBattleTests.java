3700eed2fae296c4adfd5fb2f89a8d7963f84b50
louis <Louiis1997@hotmail.fr>
2022-05-31 22:02:22 +0200

    update pom.xml and start of tests

---
+++ src/test/java/com/esgi/algoBattle/AlgoBattleTests.java
@@ -0,0 +1,13 @@
+package com.esgi.algoBattle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlgoBattleTests {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
