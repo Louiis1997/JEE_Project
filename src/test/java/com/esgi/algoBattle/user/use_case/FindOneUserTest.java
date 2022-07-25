785a29ea288d04194f89cfab5d24e2dbff6a5c6d
louis <Louiis1997@hotmail.fr>
2022-07-25 18:49:15 +0200

    try fix pipeline

--- src/test/java/com/esgi/algoBattle/user/use_case/FindOneUserTest.java
+++ src/test/java/com/esgi/algoBattle/user/use_case/FindOneUserTest.java
@@ -9,10 +9,12 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@TestPropertySource("../../../../../../resources/application.properties")
 class FindOneUserTest {
 
     @Autowired
