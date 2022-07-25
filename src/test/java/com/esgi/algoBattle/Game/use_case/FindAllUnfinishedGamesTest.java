861601b6075a06854a7a6b69b5509761269239f3
william.quach <william.quach@outlook.fr>
2022-07-25 15:15:10 +0200

    Add find all unfinished games test

---
+++ src/test/java/com/esgi/algoBattle/Game/use_case/FindAllUnfinishedGamesTest.java
@@ -0,0 +1,78 @@
+package com.esgi.algoBattle.Game.use_case;
+
+import com.esgi.algoBattle.game.domain.model.Game;
+import com.esgi.algoBattle.game.infrastructure.dataprovider.entity.GameEntity;
+import com.esgi.algoBattle.game.infrastructure.dataprovider.repository.GameRepository;
+import com.esgi.algoBattle.game.use_case.FindAllUnfinishedGames;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+public class FindAllUnfinishedGamesTest {
+
+    private Game game1;
+    private Game game2;
+    private GameEntity gameEntity1;
+    private GameEntity gameEntity2;
+
+    @Autowired
+    private FindAllUnfinishedGames findAllUnfinishedGames;
+    @MockBean
+    private GameRepository gameRepository;
+
+    @BeforeEach
+    public void initEach() {
+        game1 = new Game().setId(1L).setDate(LocalDateTime.now()).setOver(true);
+        game2 = new Game().setId(2L).setDate(LocalDateTime.now()).setOver(true);
+        gameEntity1 = new GameEntity().setId(game1.getId()).setCreatedAt(game1.getDate()).setIsOver(game1.getOver());
+        gameEntity2 = new GameEntity().setId(game2.getId()).setCreatedAt(game2.getDate()).setIsOver(game2.getOver());
+    }
+
+    @Test
+    public void when_no_unfinished_games() {
+        gameEntity1.setIsOver(true);
+        gameEntity2.setIsOver(true);
+        Mockito.when(gameRepository.findByIsOver(false)).thenReturn(List.of());
+
+        var games = findAllUnfinishedGames.execute();
+
+        Assertions.assertEquals(0, games.size());
+    }
+
+    @Test
+    public void when_one_unfinished_game() {
+        game1.setOver(false);
+        game2.setOver(true);
+        gameEntity1.setIsOver(false);
+        gameEntity2.setIsOver(true);
+        Mockito.when(gameRepository.findByIsOver(false)).thenReturn(List.of(gameEntity1));
+
+        var games = findAllUnfinishedGames.execute();
+
+        Assertions.assertEquals(1, games.size());
+        Assertions.assertEquals(game1, games.get(0));
+    }
+
+    @Test
+    public void when_two_unfinished_games() {
+        game1.setOver(false);
+        game2.setOver(false);
+        gameEntity1.setIsOver(false);
+        gameEntity2.setIsOver(false);
+        Mockito.when(gameRepository.findByIsOver(false)).thenReturn(List.of(gameEntity1, gameEntity2));
+
+        var games = findAllUnfinishedGames.execute();
+
+        Assertions.assertEquals(2, games.size());
+        Assertions.assertEquals(game1, games.get(0));
+        Assertions.assertEquals(game2, games.get(1));
+    }
+}
